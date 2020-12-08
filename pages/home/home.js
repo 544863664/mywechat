1f14d1e96ba9e09b3ad45c3ea16a73556ab6a8e7
liguodong <ueware@dell>
2020-12-08 19:12:19 +0800

    add

---
+++ pages/home/home.js
@@ -0,0 +1,161 @@
+// pages/home/home.js
+//获取应用实例
+const app = getApp()
+var slide1 = "../../images/slide1.jpg"
+var slide2 = "../../images/slide2.jpg"
+var slide3 = "../../images/slide3.jpg"
+var slide4 = "../../images/slide4.jpg"
+var slide5 = "../../images/slide5.jpg"
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        // 轮播图
+        swiper: {
+            background: [slide1, slide2, slide3, slide4, slide5],
+            indicatorDots: true,
+            vertical: false,
+            circular: true,
+            autoplay: true,
+            interval: 5000,
+            duration: 1000
+        },
+        // tabs
+        tabs: [],
+        activeTab: 0,
+        swiperHeight: "",
+
+        // icon
+        icons: [],
+
+        // 回到顶部按钮
+        scrollBtn: false,
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        this.setData({
+            search: this.search.bind(this),
+            tabs: app.globalData.home.homeTabs,
+            icons: app.globalData.home.homeIcon
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+        setTimeout(function () {
+            wx.stopPullDownRefresh()
+        }, 3000)
+    },
+    // 页面滚动监听高度
+    onPageScroll(e) {
+        this.setData({
+            scrollBtn: e.scrollTop > 200 ? true : false
+        });
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    },
+
+    /**
+     * 页面函数
+     */
+    // 头部搜索框
+    search: function (value) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve([{
+                    text: '搜索结果',
+                    value: 1
+                }, {
+                    text: '搜索结果2',
+                    value: 2
+                }])
+            }, 200)
+        })
+    },
+    // 搜索结果事件
+    selectResult: function (e) {
+        console.log('select result', e.detail)
+    },
+
+    // tabs事件
+    onTabClick(e) {
+        const index = e.detail.index
+        this.setData({
+            activeTab: index
+        })
+    },
+
+    onChange(e) {
+        const index = e.detail.index
+        this.setData({
+            activeTab: index
+        })
+    },
+
+    // 点击tab内容事件
+    handleClick(e) {
+        console.log(e);
+        wx.navigateTo({
+            url: "/pages/index/index"
+        })
+    },
+
+
+
+
+
+    // 回顶
+    scrollgotop() {
+        wx.pageScrollTo({
+            duration: 300,
+            scrollTop: 0
+        })
+    }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,105 +1,105 @@
-//index.js
+// pages/home/home.js
 //获取应用实例
 const app = getApp()
-var slide1 = "../../images/slide1.jpg"
-var slide2 = "../../images/slide2.jpg"
-var slide3 = "../../images/slide3.jpg"
-var slide4 = "../../images/slide4.jpg"
-var slide5 = "../../images/slide5.jpg"
 
 Page({
-  data: {
-    arr: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    arr2: [11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-    scrollBtn: false,
-    swiper: {
-      background: [slide1, slide2, slide3, slide4, slide5],
-      indicatorDots: true,
-      vertical: false,
-      circular: true,
-      autoplay: true,
-      interval: 5000,
-      duration: 1000
-    },
-    tabs: [{
-        title: '技术开发',
-        title2: '小程序开发进阶',
-        img: 'http://mmbiz.qpic.cn/sz_mmbiz_jpg/GEWVeJPFkSEV5QjxLDJaL6ibHLSZ02TIcve0ocPXrdTVqGGbqAmh5Mw9V7504dlEiatSvnyibibHCrVQO2GEYsJicPA/0?wx_fmt=jpeg',
-        desc: '本视频系列课程，由腾讯课堂NEXT学院与微信团队联合出品，通过实战案例，深入浅出地进行讲解。',
-      },
-      {
-        title: '产品解析',
-        title2: '微信小程序直播',
-        img: 'http://mmbiz.qpic.cn/sz_mmbiz_png/GEWVeJPFkSHALb0g5rCc4Jf5IqDfdwhWJ43I1IvriaV5uFr9fLAuv3uxHR7DQstbIxhNXFoQEcxGzWwzQUDBd6Q/0?wx_fmt=png',
-        desc: '微信小程序直播系列课程持续更新中，帮助大家更好地理解、应用微信小程序直播功能。',
-      },
-      {
-        title: '运营规范',
-        title2: '常见问题和解决方案',
-        img: 'http://mmbiz.qpic.cn/sz_mmbiz_jpg/GEWVeJPFkSGqys4ibO2a8L9nnIgH0ibjNXfbicNbZQQYfxxUpmicQglAEYQ2btVXjOhY9gRtSTCxKvAlKFek7sRUFA/0?wx_fmt=jpeg',
-        desc: '提高审核质量',
-      },
-      {
-        title: '营销经验',
-        title2: '流量主小程序',
-        img: 'http://mmbiz.qpic.cn/sz_mmbiz_jpg/GEWVeJPFkSH2Eic4Lt0HkZeEN08pWXTticVRgyNGgBVHMJwMtRhmB0hE4m4alSuwsBk3uBBOhdCr91bZlSFbYhFg/0?wx_fmt=jpeg',
-        desc: '本课程共四节，将分阶段为开发者展示如何开通流量主功能、如何接入广告组件、不同类型小程序接入的建议，以及如何通过工具调优小程序变现效率。',
-      },
-      {
-        title: '高校大赛',
-        title2: '2020中国高校计算机大赛',
-        img: 'http://mmbiz.qpic.cn/mmbiz_jpg/TcDuyasB5T3Eg34AYwjMw7xbEK2n01ekiaicPiaMInEMTkOQtuv1yke5KziaYF4MLia4IAbxlm0m5NxkibicFg4IZ92EA/0?wx_fmt=jpeg',
-        desc: '微信小程序应用开发赛',
-      },
-    ],
-    activeTab: 0,
 
-  },
-  onLoad: function () {
+  /**
+   * 页面的初始数据
+   */
+  data: {
     
   },
-  myEventListener: function (e) {
-    console.log(e)
-  },
-  onPageScroll(e) {
-    this.setData({
-      scrollBtn: e.scrollTop > 200 ? true : false
-    });
-  },
-  getUserInfo: function (e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    });
-  },
-  onPullDownRefresh() {
-    console.log("下拉刷新");
 
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      search: this.search.bind(this)
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
     setTimeout(function () {
       wx.stopPullDownRefresh()
     }, 3000)
+  },
+  // 页面滚动监听高度
+  onPageScroll(e) {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
 
   },
-  scrollgotop() {
-    wx.pageScrollTo({
-      duration: 300,
-      scrollTop: 0
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  /**
+   * 页面函数
+   */
+  // 头部搜索框
+  search: function (value) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve([{
+          text: '搜索结果',
+          value: 1
+        }, {
+          text: '搜索结果2',
+          value: 2
+        }])
+      }, 200)
     })
   },
-  // 点击 tab 时触发
-  onTabCLick(e) {
-    const index = e.detail.index;
-    this.setData({
-      activeTab: index
-    });
+  // 搜索结果事件
+  selectResult: function (e) {
+    console.log('select result', e.detail)
   },
-  // 内容区域滚动导致 tab 变化时触发
-  onChange(e) {
-    const index = e.detail.index;
-    this.setData({
-      activeTab: index
-    });
-  }
+  // 
+  myEventListener: function (e) {
+    console.log(e)
+  },
+
+
 })
